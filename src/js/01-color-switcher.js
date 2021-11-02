173864d516d26c2a24d2082c4d53892527e5ddcd
OleksandrVoitko <voytkomail@gmail.com>
2021-11-02 22:34:11 +0200

    Task 01 functionality is ready.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+let intervalId = 0;
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+stopBtnRef.setAttribute('disabled', true);
+
+function onStartBtnClick() {
+  stopBtnRef.removeAttribute('disabled');
+  startBtnRef.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    bodyRef.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  stopBtnRef.setAttribute('disabled', true);
+  startBtnRef.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
